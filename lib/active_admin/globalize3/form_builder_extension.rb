d36f5090549aff3b493291bb0dbe1f976aa096f4
mzampetti <m.zampetti@cantierecreativo.net>
2013-01-30 15:32:42 +0100

    Avoid sorting available locales

--- lib/active_admin/globalize3/form_builder_extension.rb
+++ lib/active_admin/globalize3/form_builder_extension.rb
@@ -6,7 +6,7 @@ module ActiveAdmin
       def translated_inputs(name = "Translations", &block)
         form_buffers.last << template.content_tag(:div, class: "activeadmin-translations") do
           template.content_tag(:ul, class: "available-locales") do
-            I18n.available_locales.sort.map do |locale|
+            I18n.available_locales.map do |locale|
               template.content_tag(:li) do
                 template.content_tag(:a, I18n.t(:"active_admin.globalize3.language.#{locale}"), href:".locale-#{locale}")
               end
