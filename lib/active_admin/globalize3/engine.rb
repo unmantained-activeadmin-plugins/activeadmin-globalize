dad5fb14acc47abef8a872d79723a94b8337c0fa
nebirhos <me@nebirhos.com>
2013-03-04 11:49:00 +0100

    Fixes assets precompile on Heroku

--- lib/active_admin/globalize3/engine.rb
+++ lib/active_admin/globalize3/engine.rb
@@ -1,7 +1,7 @@
 module ActiveAdmin
   module Globalize3
     class Engine < ::Rails::Engine
-      initializer "Railsyard precompile hook" do |app|
+      initializer "Railsyard precompile hook", group: :assets do |app|
         app.config.assets.precompile += [
           "active_admin/active_admin_globalize3.css",
           "active_admin/active_admin_globalize3.js"
